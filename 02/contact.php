89365bfa47bfcbdcc10af25a6e07acd863085081
nakarai <masakinito418@gmail.com>
2016-05-19 18:23:07 +0900

    a

--- 02/contact.php
+++ 02/contact.php
@@ -1,25 +1,31 @@
+<center>
+<div style="background:aqua;color:black;">
+<h3 style="font-size:x-large">お問い合わせフォーム</h3>
+<div style="background:aqua;color:black;">
 <form method="post" action="result.php">
  <p>
- 姓：<input type="text" name="姓" /><br>
- 名：<input type="text" name="名" /><br>
- 電話番号：<input type="text" name="1"/>　<input type="text" name="2"/>　<input type="text" name="3"/><br>
+
+ 姓：<input type="text" name="姓" size="5"/><br><br>
+ 名：<input type="text" name="名" size="5"/><br><br>
+ 性別：<input type="radio" name="性別" value="男" checked="checked" /> 男　
+ <input type="radio" name="性別" value="女" /> 女 <input type="radio" name="性別" value="不明" checked="checked" /> 不明<br><br>
+ 住所：<input type="text" name="住所" size=40/><br><br>
+ 電話番号：<input type="text" name="1" size="4"/>-<input type="text" name="2" size="4"/>-<input type="text" name="3" size="4"/><br><br>
+ メールアドレス：<input type="text" name="メアド前" />＠<input type="text" name="メアド後" /><br><br>
+ どこで知ったか：
+ <input type="checkbox" name="where[]" value="ネット" /> ネット
+ <input type="checkbox" name="where[]" value="口コミ" /> 口コミ
+ <input type="checkbox" name="where[]" value="その辺で" /> その辺で<br><br>
  質問カテゴリ：
  <select name="質問カテゴリ">
    <option>クレーム</option>
    <option>プレデター</option>
    <option>ラブレター</option>
    <option>適当</option>
- </select><br>
- メールアドレス：<input type="text" name="メアド前" />＠<input type="text" name="メアド後" /><br>
- 性別：<input type="radio" name="性別" value="男" checked="checked" /> 男　
- <input type="radio" name="性別" value="女" /> 女<br>
- どこで知ったか：
- <input type="checkbox" name="properties" value="ネット" /> ネット
- <input type="checkbox" name="properties" value="口コミ" /> 口コミ
- <input type="checkbox" name="properties" value="その辺で" /> その辺で<br>　
- <textarea name="freeans" rows="4" cols="40">自由に意見を記述してください</textarea><br>
- 住所：<input type="text" name="住所" /><br>
+ </select><br><br>　
+ 質問内容：<textarea name="freeans" rows="4" cols="40">質問を記入してください。</textarea><br><br>
  <input type="submit" value="送信" />　
  <input type="reset" value="取り消し" />
  </p>
 </form>
+<center/>
--- 02/result.php
+++ 02/result.php
@@ -1,4 +1,15 @@
 <?php
 echo "氏名：".$_POST["姓"]." ".$_POST["名"]."<br/>";
-echo "電話番号：".$_POST["1"]." ".$_POST["2"]."\n";
+echo "性別：".$_POST["性別"]."<br/>";
+echo "住所：".$_POST["住所"]."<br/>";
+echo "電話番号：".$_POST["1"]."-".$_POST["2"]."-".$_POST["3"]."<br/>";
+echo "メールアドレス：".$_POST["メアド前"].$_POST["メアド後"]."<br/>";
+$checkbox = $_POST["where"];
+echo "どこで知ったか：";
+for($i=0; $i<sizeof($checkbox); $i++){
+echo "${checkbox[$i]}".",";
+}
+echo "<br/>";
+echo "質問カテゴリ：".$_POST["質問カテゴリ"]."<br/>";
+echo "質問内容：".$_POST["freeans"]."<br/>";
 ?>
