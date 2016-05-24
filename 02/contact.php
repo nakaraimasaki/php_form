3c0c652f6477a88098fc9697540448bf89e1f237
nakarai <masakinito418@gmail.com>
2016-05-24 18:00:38 +0900

    a

--- 02/contact.php
+++ 02/contact.php
@@ -9,46 +9,35 @@
 
 
 
+
 <form method="post" action="result.php">
 <p class="box"></p>
 <p>
 <div class="sample" >
- <div class="note" >
 <ul>
-
- <li><span>姓(5文字まで)：</span></li>
- <input type="text" name="姓" size="5"/><br><br>
- <li><span>名：(5文字まで)</span></li>
- <input type="text" name="名" size="5"/><br><br>
- <li><span>性別：</span></li>
- <input type="radio" name="性別" value="男" checked="checked" />男　
- <input type="radio" name="性別" value="女" />女
- <input type="radio" name="性別" value="不明" checked="checked" />不明<br><br>
- <li><span>住所：</span></li>
- <input type="text" name="住所" size=40/><br><br>
- <li><span>電話番号：</span></li>
- <input type="text" name="1" size="4"/>
- -<input type="text" name="2" size="4"/>
- -<input type="text" name="3" size="4"/><br><br>
- <li><span>メールアドレス：</span></li>
- <input type="text" name="メアド前" />＠<input type="text" name="メアド後" /><br><br>
- <li><span>どこで知ったか(必須)：</span></li>
- <input type="checkbox" name="where[]" value="ネット" />ネット
- <input type="checkbox" name="where[]" value="口コミ" />口コミ
- <input type="checkbox" name="where[]" value="その辺で" />その辺で<br><br>
- <li><span>質問カテゴリ：</span></li>
- <select name="質問カテゴリ">
+ <h1><li><span>姓(5文字まで)：</span></li><input type="text" name="sei" size="5" style="WIDTH: 200px; HEIGHT: 40px"/></h1>
+ <h1><li><span>名(5文字まで)：</span></li><input type="text" name="名" size="5" style="WIDTH: 200px; HEIGHT: 40px"/></h1>
+ <h1><li><span>性別：</span></li><input type="radio" name="性別" value="男" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"/>男
+ <input type="radio" name="性別" value="女" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"/>女
+　<input type="radio" name="性別" value="不明" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"/>不明</h1><h1><li><span>住所：</span></li><input type="text" name="住所" size="5" style="WIDTH: 200px; HEIGHT: 40px"/></h1>
+　<h1><li><span>電話番号：</span></li><input type="text" name="1" size="4" style="WIDTH: 60px; HEIGHT: 40px"/>-
+ <input type="text" name="2" size="4" style="WIDTH: 60px; HEIGHT: 40px"/>-
+ <input type="text" name="3" size="4" style="WIDTH: 60px; HEIGHT: 40px"/></h1>
+ <h1><li><span>メールアドレス：</span></li><input type="text" name="メアド前" size="40" style="WIDTH: 120px; HEIGHT: 40px"/>＠
+ <input type="text" name="メアド後" size="20" style="WIDTH: 120px; HEIGHT: 40px"/></h1>
+ <h1><li><span>どこで知ったか(必須)：</span></li><input type="checkbox" name="where[]" value="ネット" style="WIDTH: 40px; HEIGHT: 40px"/>ネット
+ <input type="checkbox" name="where[]" value="口コミ" style="WIDTH: 40px; HEIGHT: 40px"/>口コミ</h1>
+ <h1><li><span>質問カテゴリ：</span></li>
+ <select name="質問カテゴリ" style="WIDTH: 150px; HEIGHT: 40px">
    <option>クレーム</option>
    <option>プレデター</option>
    <option>ラブレター</option>
    <option>適当</option>
- </select><br><br>　
- <li><span>質問内容：</span></li>
- <textarea name="freeans" rows="4" cols="40">質問を記入してください。</textarea><br><br>
- <input type="submit" value="送信" />　
- <input type="reset" value="取り消し" />
+ </select></h1>　<h1><li><span>質問内容：</span></li>
+ <textarea name="freeans" rows="4" cols="40">質問を記入してください。</textarea></h1><br>
+ <input type="submit" value="送信" style="WIDTH: 400px; HEIGHT: 60px" />　
+ <input type="reset" value="取り消し" style="WIDTH: 200px; HEIGHT: 60px" />
  </ul>
- </div>
  </div>
  </h4>
  </p>
--- 02/result.php
+++ 02/result.php
@@ -1,23 +1,24 @@
 <center>
+
 <link rel="stylesheet" href="form.css" type="text/css">
 
-<h3 style="font-size:xx-large">こちらの内容でよろしいでしょうか？</h3>
+<h3 style="font-size:xx-large">お問い合わせフォーム</h3>
 
-<form method="post" action="end.php">
+    <form method="post" action="end.php">
     <p class="box"></p>
     <p>
     <div class="sample" >
-     <div class="note" >
-         <input type="submit" value="送信" />　
-         <input type="reset" value="取り消し" />
-</div>
-</div>
-</h4>
-</p>
-</form>
-</center>
+        <h2>下記の内容でよろしいでしょうか？</h2><br>
+<h1>
 
-php?>
-echo "質問カテゴリ：".$_POST["質問カテゴリ"]."<br/>";
-echo "質問内容：".$_POST["freeans"]."<br/>";
+ <?php
+    echo "姓：".$_POST["sei"]." "."名：".$_POST["名"]."<br/>"."<br/>";
+    echo "性別：".$_POST["性別"]."<br/>"."<br/>";
+    echo "電話番号：".$_POST["1"]."-".$_POST["2"]."-".$_POST["3"]."<br/>"."<br/>";
+    echo "メールアドレス：".$_POST["メアド前"]."＠".$_POST["メアド後"]."<br/>"."<br/>";
+    echo "質問カテゴリ：".$_POST["質問カテゴリ"]."<br/>"."<br/>";
+    echo "質問内容：".$_POST["freeans"]."<br/>";
 ?>
+</h1>
+
+</center>
