05aa55b5c3981392ef83019c322729fa115d1cb9
nakarai <masakinito418@gmail.com>
2016-05-26 14:15:15 +0900

    a

--- 02/contact.php
+++ 02/contact.php
@@ -12,44 +12,46 @@
 <ul>
 
  <h1>
-     <li><span>姓：</span>
-     </li><input type="text" name="sei" placeholder="例:なんとか" size="50000" style="WIDTH: 150px; HEIGHT: 40px;font-size:30px"/>
+     <li><span>姓：</span></li>
+     <input type="text" name="sei" placeholder="例:なんとか" size="5" maxlength="5" style="WIDTH: 150px; HEIGHT: 40px;font-size:30px" required/>
  </h1>
 
  <h1>
      <li><span>名：</span></li>
-     <input type="text" name="名" placeholder="例:なんとか"　size="5" maxlength="3" style="WIDTH: 150px; HEIGHT: 40px;font-size:30px"/>
+     <input type="text" name="名" placeholder="例:なんとか"　size="5" maxlength="5" style="WIDTH: 150px; HEIGHT: 40px;font-size:30px"required/>
  </h1>
 
  <h1>
      <li><span>性別：</span></li>
-     <input type="radio" name="性別" value="男" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"/>男
-     <input type="radio" name="性別" value="女" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"/>女
-　    <input type="radio" name="性別" value="不明" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"/>不明
+     <input type="radio" name="性別" value="男" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"required/>男
+     <input type="radio" name="性別" value="女" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"required/>女
+　    <input type="radio" name="性別" value="不明" checked="checked" style="WIDTH: 40px; HEIGHT: 40px"required/>不明
 </h1>
 
 <h1>
     <li><span>住所：</span></li>
-    <input type="text" name="住所" placeholder="例:その辺" size="2" style="WIDTH: 900px; HEIGHT: 40px;font-size:30px"/>
+    <input type="text" name="住所" placeholder="例:その辺" size="40" maxlength="40"style="WIDTH: 900px; HEIGHT: 40px;font-size:30px"required/>
 </h1>
 
 　<h1>
     <li><span>電話番号：</span></li>
-    <input type="text" name="1" placeholder="例:0000"　size="2" style="WIDTH: 120px; HEIGHT: 40px;font-size:30px"/>-
-    <input type="text" name="2" placeholder="例:1111" size="4" style="WIDTH: 120px; HEIGHT: 40px;font-size:30px"/>-
-    <input type="text" name="3" placeholder="例:2222" size="4" style="WIDTH: 120px; HEIGHT: 40px;font-size:30px"/>
+    <input type="text" name="1" placeholder="例:0000"　size="2" maxlength="4" style="WIDTH: 120px; HEIGHT: 40px;font-size:30px"required/>-
+    <input type="text" name="2" placeholder="例:1111" size="4" maxlength="4" style="WIDTH: 120px; HEIGHT: 40px;font-size:30px"required/>-
+    <input type="text" name="3" placeholder="例:2222" size="4" maxlength="4" style="WIDTH: 120px; HEIGHT: 40px;font-size:30px"required/>
 </h1>
 
  <h1>
      <li><span>メールアドレス：</span></li>
-     <input type="text" name="メアド前"  placeholder="例:なんとかなんとか" size="40" style="WIDTH: 600px; HEIGHT: 40px;font-size:30px"/>＠
-     <input type="text" name="メアド後"  placeholder="例:なんとかなんとか" size="20" style="WIDTH: 300px; HEIGHT: 40px;font-size:30px"/>
+     <input type="text" name="メアド前"  placeholder="例:なんとかなんとか" size="40" maxlength="35" style="WIDTH: 600px; HEIGHT: 40px;font-size:30px"required/>＠
+     <input type="text" name="メアド後"  placeholder="例:なんとかなんとか" size="20" maxlength="19" style="WIDTH: 300px; HEIGHT: 40px;font-size:30px"required/>
  </h1>
 
  <h1>
      <li><span>どこで知ったか(必須)：</span></li>
      <label><input type="checkbox" name="where[]" value="1" style="WIDTH: 40px; HEIGHT: 40px"/>ネット</label>
      <label><input type="checkbox" name="where[]" value="2" style="WIDTH: 40px; HEIGHT: 40px"/>口コミ</label>
+     <label><input type="checkbox" name="where[]" value="3" style="WIDTH: 40px; HEIGHT: 40px"/>あの辺で</label>
+     <label><input type="checkbox" name="where[]" value="5" style="WIDTH: 40px; HEIGHT: 40px"/>その辺で</label>
 </h1>
 
  <h1>
@@ -64,11 +66,11 @@
 
 <h1>
     <li><span>質問内容：</span></li>
-    <textarea name="freeans"  placeholder="例:なんとかなんとか" rows="4" cols="60" style="font-size:30px"></textarea>
+    <textarea name="freeans"  placeholder="例:なんとかなんとか" rows="4" cols="60" maxlength="120" style="font-size:30px" required></textarea>
 </h1><br>
 
- <input class="button" type="submit" value="送信" style="WIDTH: 600px; HEIGHT: 60px" />　
- <input class="button" type="reset" value="リセット" style="WIDTH: 200px; HEIGHT: 60px" />
+ <input class="button" type="submit" value="送信" style="WIDTH: 600px; HEIGHT: 60px"/>　
+ <input class="button" type="reset" value="リセット" style="WIDTH: 200px; HEIGHT: 60px"/>
  </ul>
  </div>
  </h4>
--- 02/result.php
+++ 02/result.php
@@ -9,7 +9,7 @@
     <form method="post" action="end.php">
     <p class="box"></p>
     <p>
-    <div class="sample" >
+    <div class="sample1" >
         <h2>下記の内容でよろしいでしょうか？</h2><br>
 <ul>
         <h1>
@@ -21,31 +21,55 @@
               <li><span>性別：</span></li>
               <?php echo $_POST["性別"];?>
           </h1>
- <?php
-    echo "氏名：".$_POST["sei"]." ".$_POST["名"]."<br/>"."<br/>";
-    echo "性別：".$_POST["性別"]."<br/>"."<br/>";
-    echo "住所：".$_POST["住所"]."<br/>"."<br/>";
-    echo "電話番号：".$_POST["1"]."-".$_POST["2"]."-".$_POST["3"]."<br/>"."<br/>";
-    echo "メールアドレス：".$_POST["メアド前"]."＠".$_POST["メアド後"]."<br/>"."<br/>";
-    echo "どこで知ったか：";
-    if(empty($_POST['where'])==false){
-        foreach ($_POST['where'] as $value) {
-            switch (htmlspecialchars($value)) {
-                case '1':
-                    echo "ネット".",";
-                    break;
-                case '2':
-                    echo "口コミ";
-                    break;
-            }
-        }
-    }
-    echo "<br/>"."<br/>";
 
-    echo "質問カテゴリ：".$_POST["質問カテゴリ"]."<br/>"."<br/>";
-    echo "質問内容：".$_POST["freeans"]."<br/>"."<br/>"."<br/>"."<br/>"."<br/>"."<br/>";
-?>
-<input class="button" type="submit" value="送信" style="WIDTH: 200px; HEIGHT: 60px" />　
+          <h1>
+               <li><span>住所：</span></li>
+               <?php echo $_POST["住所"];?>
+           </h1>
+
+           <h1>
+                <li><span>電話番号：</span></li>
+                <?php echo $_POST["1"]."-".$_POST["2"]."-".$_POST["3"];?>
+            </h1>
+
+            <h1>
+                 <li><span>メールアドレス：</span></li>
+                 <?php echo $_POST["メアド前"]."＠".$_POST["メアド後"];?>
+             </h1>
+
+             <h1>
+                  <li><span>どこで知ったか：</span></li>
+                  <?php if(empty($_POST['where'])==false){
+                      foreach ($_POST['where'] as $value) {
+                          switch (htmlspecialchars($value)) {
+                              case '1':
+                                  echo "ネット".",";
+                                  break;
+                              case '2':
+                                  echo "口コミ".",";
+                                  break;
+                                  case '3':
+                                        echo "あの辺で".",";
+                                        break;
+                                          case '5':
+                                                echo "その辺で";
+                                                break;
+                          }
+                      }
+                  }
+                  ?>
+              </h1>
+
+              <h1>
+                   <li><span>質問カテゴリ：</span></li>
+                   <?php echo $_POST["質問カテゴリ"];?>
+               </h1>
+
+               <h1>
+                    <li><span>質問内容：</span></li>
+                    <?php echo $_POST["freeans"];?>
+                </h1>
+<input class="button" type="submit" value="送信" style="WIDTH: 600px; HEIGHT: 60px" />　
 <INPUT class="button" type="button" value="戻る" onClick="history.back()" style="WIDTH: 200px; HEIGHT: 60px">
 </h1>
 
